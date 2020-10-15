52b23546860379885909b02a04561d84afc373c5
Denis Matrov <jazzhole|@gmail.com>
2020-10-15 16:40:16 +0300

    I changed the imputation method for food data to `2l.pan` because of the convergence issues with other algorithms. AMIS items are imputed by `2l.pmm` algorithm.

--- R/imputation_mice.R
+++ R/imputation_mice.R
@@ -4,16 +4,20 @@ library(blme)
 library(here)
 library(mice)
 library(miceadds)
+library(micemd)
 ################################################################################
 path <- here("mod_data", "df_for_imputation.csv")
 df <- read_csv(path)
 df <- df %>% dplyr::mutate(sugu = factor(sugu))
-df_cohort1 <- df %>% dplyr::filter(kohort == 1) %>% dplyr::select(-kohort)
-df_cohort2 <- df %>% dplyr::filter(kohort == 2) %>% dplyr::select(-kohort)
+# The continuous data is centered to improve the stability of the estimates
+df_cohort1 <- df %>% dplyr::filter(kohort == 1) %>% dplyr::select(-kohort) %>% 
+  mutate_at(4:43, ~(scale(.x, scale = TRUE) %>% as.vector())) 
+df_cohort2 <- df %>% dplyr::filter(kohort == 2) %>% dplyr::select(-kohort) %>% 
+  mutate_at(4:43, ~(scale(.x, scale = TRUE) %>% as.vector()))
 ################################################################################
 # make a separate block for AMIS questionnaire items
 block_AMIS <- mice::make.blocks(df_cohort1[, c(1:3, 44:67)], partition = "collect")
-block_food <- mice::make.blocks(df_cohort1[, -c(44:67)], partition = "collect")
+block_food <- mice::make.blocks(df_cohort1[, -c(44:67)], partition = "scatter")
 blocks = c(block_food, block_AMIS)
 ################################################################################
 # imputation
@@ -22,18 +26,26 @@ path_imp <- here("imputed_data", "mice_imp_cohort1.rds")
 imp_cohort1 <- mice(df_cohort1, blocks = blocks, method = "2l.pmm", maxit = 0)
 pred_matrix <- imp_cohort1$predictorMatrix
 pred_matrix[, c("kood")] <- pred_matrix[, c("kood")] * -2
-imp_cohort1 <- mice(df_cohort1, blocks = blocks, method = "2l.pmm", m = 100,
-                    maxit = 100, blme_use = TRUE,
-                    predictorMatrix = pred_matrix, printFlag = TRUE)
+
+# write predictor matrix to hard drive to modify it in spreadsheet software
+path <- here("imputed_data", "mice_pred_matrix.csv")
+# write_csv(as.data.frame(pred_matrix), path)
+mod_matrix <- read_csv2(path) %>% as.matrix()
+dimnames(mod_matrix) <- dimnames(pred_matrix) 
+meth <- imp_cohort1$method
+# use "2l.norm" for rest of the data and "2l.pmm" for AMIS scale items
+meth[4:43] <- "2l.pan"
+imp_cohort1 <- mice(df_cohort1, blocks = blocks, method = meth, m = 100,
+                    maxit = 40,
+                    predictorMatrix = mod_matrix, printFlag = TRUE, seed = 1984)
+plot(imp_cohort1)
 saveRDS(imp_cohort1, path_imp)
 ################################################################################
 # cohort 2
 path_imp <- here("imputed_data", "mice_imp_cohort2.rds")
-imp_cohort2 <- mice(df_cohort2, blocks = blocks, method = "2l.pmm", maxit = 0)
-pred_matrix <- imp_cohort2$predictorMatrix
-pred_matrix[, c("kood")] <- pred_matrix[, c("kood")] * -2
-imp_cohort2 <- mice(df_cohort2, blocks = blocks, method = "2l.pmm", m = 100,
-                    maxit = 100, blme_use = TRUE,
-                    predictorMatrix = pred_matrix, printFlag = TRUE)
+imp_cohort2 <- mice(df_cohort2, blocks = blocks, method = meth, m = 100,
+                    maxit = 40, 
+                    predictorMatrix = mod_matrix, printFlag = TRUE, seed == 1984)
+plot(imp_cohort2)
 saveRDS(imp_cohort2, path_imp)
 ################################################################################
