52f5753e0c0906496cc3f25f1139fbc2fa71b41e
Denis Matrov <jazzhole@gmail.com>
2020-10-23 13:53:00 +0300

    Modified Blimp imputation routine because the previous way was non-convergent

--- preliminary/data_prep_Blimp_imp_redNA.R
+++ preliminary/data_prep_Blimp_imp_redNA.R
@@ -22,3 +22,34 @@ path_c2 <- here("mod_data", "df_for_imputation_blimp_c2f.csv")
 
 write_csv(df_c1_filt, path_c1, na = "-999", col_names = FALSE)
 write_csv(df_c2_filt, path_c2, na = "-999", col_names = FALSE)
+
+# because the imputation of the file with the full set of variables has failed, 
+# I will AMIS and food data separately.
+# to employ impute by group feature in Blimp
+# I selected age, gender, Wmaxkg, kcal, BMI, and AMIS items for initial imputation
+df_AMIS_c1 <- df_c1_filt %>% 
+  dplyr::select(1:4, 15:16, 44:67) %>% 
+  dplyr::mutate_at(4:6, ~(scale(.x, scale = TRUE) %>% as.vector()))
+df_AMIS_c2 <- df_c2_filt %>% 
+  dplyr::select(1:4, 15:16, 44:67) %>% 
+  dplyr::mutate_at(4:6, ~(scale(.x, scale = TRUE) %>% as.vector()))
+path_c1_AMIS <- here("mod_data", "df_for_imputation_blimp_c1f_AMIS.csv")
+path_c2_AMIS <- here("mod_data", "df_for_imputation_blimp_c2f_AMIS.csv")
+
+# the rest of the data to impute without AMIS items
+df_food_c1 <- df_c1_filt %>% 
+  dplyr::select(-c(44:67)) %>% 
+  dplyr::mutate_at(4:43, ~(scale(.x, scale = TRUE) %>% as.vector()))
+df_food_c2 <- df_c2_filt %>% 
+  dplyr::select(-c(44:67)) %>% 
+  dplyr::mutate_at(4:43, ~(scale(.x, scale = TRUE) %>% as.vector()))
+  
+path_c1_food <- here("mod_data", "df_for_imputation_blimp_c1f_food.csv")
+path_c2_food <- here("mod_data", "df_for_imputation_blimp_c2f_food.csv")
+
+write_csv(df_AMIS_c1, path_c1_AMIS, na = "-999", col_names = FALSE)
+write_csv(df_AMIS_c2, path_c2_AMIS, na = "-999", col_names = FALSE)
+
+write_csv(df_food_c1, path_c1_food, na = "-999", col_names = FALSE)
+write_csv(df_food_c2, path_c2_food, na = "-999", col_names = FALSE)
+
