7caf1142313163bfbe683a20350ea829dc8a54d6
Denis Matrov <jazzhole|@gmail.com>
2020-11-12 11:14:24 +0200

    changes have been made to power analysis code

--- R/lmer_power_sim.R
+++ R/lmer_power_sim.R
@@ -26,20 +26,20 @@ lmer_aImp <- lmer(aImp ~ Zink + Cerealprod + VitB6 + mImp + age_cent + sugu +
 
 ################################################################################
 # power analysis
-# Alco has the lowest coefficent for mImp
-# Zink has the lowest coefficent for aImp
+# Alco has the lowest coefficient for mImp
+# Zink has the lowest coefficient for aImp
 # mImp
-power_mImp_AlCo <- powerSim(lmer_mImp, test = fixed("Alco", "pb"), nsim = 1000)
+power_mImp_AlCo <- powerSim(lmer_mImp, test = fixed("Alco", "pb"), nsim = 500)
 power_mImp_Sodium_sugu <- powerSim(lmer_mImp, 
                                    test = fcompare(~ Wmaxkg + Selenium + aImp + 
                                                      Sodium + sugu + Zink + Fish +
                                                      Veget + Alco +
-                                                     age_cent, "pb"), nsim = 1000)
+                                                     age_cent, "pb"), nsim = 500)
 # aImp
-power_aImp_Zink <- powerSim(lmer_aImp, test = fixed("Zink", "pb"), nsim = 1000)
+power_aImp_Zink <- powerSim(lmer_aImp, test = fixed("Zink", "pb"), nsim = 500)
 power_aImp_age_sugu <- powerSim(lmer_aImp, 
                                    test = fcompare(~ Zink + Cerealprod + VitB6 +
                                                      mImp + age_cent + 
-                                                     sugu , "pb"), nsim = 1000)
-
+                                                     sugu , "pb"), nsim = 500)
+power_aImp_VitB6 <- powerSim(lmer_aImp, test = fixed("VitB6", "pb"), nsim = 500)
 ################################################################################
